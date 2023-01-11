300053955e74442eae546059028c65005c3a3c1c
OneTrick475 <ibukev4@gmail.com>
2023-01-11 15:26:21 +0200

    added required info headers

--- Scrabble/Scrabble.h
+++ Scrabble/Scrabble.h
@@ -1,3 +1,18 @@
+/**
+*
+* Solution to course project #8
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Ivan Bukev
+* @idnumber 2MI0600196
+* @compiler VC
+*
+* header file for the Scrabble struct
+*
+*/
+
 #ifndef SCRABBLE_H
 #define SCRABBLE_H
 
--- Scrabble/Source.cpp
+++ Scrabble/Source.cpp
@@ -1,5 +1,20 @@
-#include "Scrabble.h"
+/**
+*
+* Solution to course project #8
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Ivan Bukev
+* @idnumber 2MI0600196
+* @compiler VC
+*
+* source file
+*
+*/
 
+
+#include "Scrabble.h"
 
 int main(){
 	Scrabble scrabble;
